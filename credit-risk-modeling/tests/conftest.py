caad7cab2756771cf8ba6f7d80fe2f38dded4df6
gabriel <gfluz94@gmail.com>
2023-02-05 18:01:58 -0300

    add unit tests for TimeSinceCalculator

--- credit-risk-modeling/tests/conftest.py
+++ credit-risk-modeling/tests/conftest.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict
+from datetime import datetime
 import pytest
 
 
@@ -22,7 +23,7 @@ def dummy_input_dict() -> Dict[str, Any]:
         "addr_state": "AZ",
         "dti": 27.65,
         "delinq_2yrs": 0.0,
-        "earliest_cr_line": "Jan-65",
+        "earliest_cr_line": "Jan-95",
         "verification_status": "Verified",
         "initial_list_status": "f",
         "inq_last_6mths": 1.0,
@@ -33,4 +34,13 @@ def dummy_input_dict() -> Dict[str, Any]:
         "total_rev_hi_lim": None,
         "months_since_issue_date": 72.01790591182571,
         "months_since_earliest_cr_line": 394.98141645619006,
+    }
+
+
+@pytest.fixture(scope="module")
+def time_since_input_dict() -> Dict[str, Any]:
+    return {
+        "id": 1077501,
+        "member_id": 1296599,
+        "earliest_cr_line": datetime.strptime("Jan-95", "%b-%y"),
     }
---
+++ credit-risk-modeling/tests/test_feature_engineering/test_compute_time_since.py
@@ -0,0 +1,79 @@
+from typing import Any, Dict
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import pytest
+
+from credit_risk_modeling.feature_engineering import TimeSinceCalculator
+from credit_risk_modeling.exceptions.base import DataTypeNotAllowedForTransformation
+from credit_risk_modeling.exceptions.cleaning import FieldNotFound
+from credit_risk_modeling.exceptions.feature_engineering import TimeUnitNotAvailable
+
+
+class TestTimeSinceCalculator(object):
+    _REFERENCE_DATE = datetime(2017, 12, 1)
+
+    def test_transform_raisesDataTypeNotAllowedForTransformation(self):
+        base_transformer = TimeSinceCalculator(
+            field_name="earliest_cr_line",
+            reference_date=self._REFERENCE_DATE,
+            time_unit="month",
+            winsorize_max=True,
+        )
+        with pytest.raises(DataTypeNotAllowedForTransformation):
+            base_transformer.transform(np.ones(shape=10))
+
+    def test_transform_raisesFieldNotFound(self, time_since_input_dict: Dict[str, Any]):
+        base_transformer = TimeSinceCalculator(
+            field_name="NON_EXISTING_FIELD",
+            reference_date=self._REFERENCE_DATE,
+            time_unit="month",
+            winsorize_max=True,
+        )
+        with pytest.raises(FieldNotFound):
+            base_transformer.transform(time_since_input_dict)
+
+    def test_transform_raisesTimeUnitNotAvailable(
+        self, time_since_input_dict: Dict[str, Any]
+    ):
+        with pytest.raises(TimeUnitNotAvailable):
+            _ = TimeSinceCalculator(
+                field_name="earliest_cr_line",
+                reference_date=self._REFERENCE_DATE,
+                time_unit="second",
+                winsorize_max=True,
+            )
+
+    def test_transform_outputsDictionary(self, time_since_input_dict: Dict[str, Any]):
+        # OUTPUT
+        base_transformer = TimeSinceCalculator(
+            field_name="earliest_cr_line",
+            reference_date=self._REFERENCE_DATE,
+            time_unit="month",
+            winsorize_max=True,
+        )
+        output = base_transformer.transform(time_since_input_dict)
+
+        # EXPECTED
+        expected = time_since_input_dict.copy()
+        expected["months_since_earliest_cr_line"] = 274.9953797819257
+
+        # ASSERT
+        expected == output
+
+    def test_transform_outputsDataFrame(self, time_since_input_dict: Dict[str, Any]):
+        # OUTPUT
+        base_transformer = TimeSinceCalculator(
+            field_name="earliest_cr_line",
+            reference_date=datetime(2017, 12, 1),
+            time_unit="month",
+            winsorize_max=True,
+        )
+        output = base_transformer.transform(pd.DataFrame([time_since_input_dict]))
+
+        # EXPECTED
+        expected = pd.DataFrame([time_since_input_dict])
+        expected["months_since_earliest_cr_line"] = 274.9953797819257
+
+        # ASSERT
+        pd.testing.assert_frame_equal(expected, output)
